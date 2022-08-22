7f1bf353789e1fb169f7915a7071b0f7a8ebb92e
Gabriel Gonçalves <gagsilva28@gmail.com>
2022-08-22 16:17:46 -0300

    Create Account

--- src/Pages/CreateAccount/CreateAccount.tsx
+++ src/Pages/CreateAccount/CreateAccount.tsx
@@ -1,5 +1,6 @@
-import { Component } from "react";
+import { useContext, useState } from "react";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 import "./CreateAccount.scss";
 
@@ -13,65 +14,70 @@ import FormBox from "../../Components/Utils/FormBox/FormBox";
 import Link from "../../Components/Utils/Link/Link";
 import { useRequest } from "../../Hooks/useRequest";
 
-class CreateAccount extends Component {
-	state = {
+type PageState = {
+	form: {
+		email: string;
+		password: string;
+		name: string;
+	};
+	loading: boolean;
+};
+
+const CreateAccount = (props: any) => {
+	const [state, setState] = useState<PageState>({
 		form: {
 			email: "",
 			password: "",
 			name: ""
 		},
 		loading: false
-	};
+	});
+	const navigate = useNavigate();
 
-	constructor(props: any) {
-		super(props);
-
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	async handleSubmit(e: any) {
+	const handleSubmit = async (e: any) => {
 		e.preventDefault();
 		const { post } = useRequest();
 
-		this.setState({ loading: true });
-		const response = await post("/user/create", this.state.form);
-		this.setState({ loading: false });
+		setState({ ...state, loading: true });
+		const response = await post("/user/create", state.form);
+		setState({ ...state, loading: false });
 
 		if (response.isOk) {
-			return toast.success("Seu usuário foi criado! Seja bem-vindo ao Sign What?", {});
+			toast.success("Seu usuário foi criado! Seja bem-vindo ao Sign What?", {});
+			navigate("/login");
+		} else {
+			toast.error("Não foi possível criar seu usuário", {});
 		}
+	};
 
-		return toast.error('Não foi possível criar seu usuário', {});
-	}
+	const handleChange = (e: any) => {
+		setState({ ...state, form: { ...state.form, [e.name]: e.value } });
+	};
 
-	handleChange(e: any) {
-		this.setState({ form: { ...this.state.form, [e.name]: e.value } });
-	}
-	render() {
-		return (
-			<div className="flex items-center justify-center h-screen bg-default">
-				<FormBox sideImage={rightImage}>
-					<div className="flex flex-col items-center title">
-						<img className="logo" src={signWhatLogo} alt="SignWhat" />
+	return (
+		<div className="flex items-center justify-center h-screen bg-default">
+			<FormBox sideImage={rightImage}>
+				<div className="flex flex-col items-center title">
+					<img className="logo" src={signWhatLogo} alt="SignWhat" />
+				</div>
+				<form className="flex flex-col items-end justify-start" onSubmit={handleSubmit}>
+					<h3 className="form-title">Crie sua conta</h3>
+					<Input onChange={handleChange} required name="name" label="Nome completo" placeholder="Digite seu nome completo" type="text" />
+					<Input onChange={handleChange} required name="email" label="E-mail" placeholder="exemplo@email.com" type="email" />
+					<Input onChange={handleChange} required name="password" label="Senha" placeholder="Crie sua senha" type="password" />
+					<Button type="submit" loading={state.loading}>
+						Criar conta
+					</Button>
+
+					<div className="create-account">
+						<Link href="/login">
+							<span> Já tem uma conta? </span> Faça login
+						</Link>
 					</div>
-					<form className="flex flex-col items-end justify-start" onSubmit={this.handleSubmit}>
-						<h3 className="form-title">Crie sua conta</h3>
-						<Input onChange={this.handleChange} required name="name" label="Nome completo" placeholder="Digite seu nome completo" type="text" />
-						<Input onChange={this.handleChange} required name="email" label="E-mail" placeholder="exemplo@email.com" type="email" />
-						<Input onChange={this.handleChange} required name="password" label="Senha" placeholder="Crie sua senha" type="password" />
-						<Button type="submit" loading={this.state.loading}>Criar conta</Button>
-
-						<div className="create-account">
-							<Link href="/login">
-								<span> Já tem uma conta? </span> Faça login
-							</Link>
-						</div>
-					</form>
-				</FormBox>
-			</div>
-		);
-	}
-}
+				</form>
+			</FormBox>
+		</div>
+	);
+};
 
 export default CreateAccount;
