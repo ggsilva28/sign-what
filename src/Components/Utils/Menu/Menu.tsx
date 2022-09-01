b44a4bbe26c065f186e33060c7ac759c38b4450c
Gabriel Gonçalves <gagsilva28@gmail.com>
2022-09-01 18:33:28 -0300

    Menu shrink and animation

--- src/Components/Utils/Menu/Menu.tsx
+++ src/Components/Utils/Menu/Menu.tsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { createRef, useContext, useRef, useState } from "react";
 import { faList, faGear, faUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 import "./Menu.scss";
 
 //Images
 import signWhatLogo from "../../../assets/img/sign-what.png";
+import signWhatOverLogo from "../../../assets/img/sign-what-over.png";
 
 //Components
 import MenuItem from "../MenuItem/MenuItem";
@@ -12,16 +13,30 @@ import { AuthContext, AuthContextData } from "../../../Context/auth";
 
 const Menu = (props: any) => {
 	const context = useContext(AuthContext);
+	const [classList, setClassList] = useState(['menu-container ', 'shrink'])
 
 	const logout = () => {
 		const { signOut } = context as AuthContextData;
 		signOut();
 	};
 
+	const openMenu = () => {
+		setTimeout(() => {
+			setClassList(['menu-container '])
+		}, 200);
+	};
+
+	const closeMenu = () => {
+		setTimeout(() => {
+			setClassList(['menu-container ', 'shrink'])
+		}, 250);
+	};
+
 	return (
-		<div className="menu-container">
+		<div className={classList.join().replaceAll(',', '')} onMouseOver={openMenu} onMouseLeave={closeMenu}>
 			<div className="logo item">
-				<img src={signWhatLogo} alt="Sign What Logo" title="Sign What?" />
+				<img className="logo-line" src={signWhatLogo} alt="Sign What Logo" title="Sign What?" />
+				<img className="logo-over" src={signWhatOverLogo} alt="Sign What Logo" title="Sign What?" />
 			</div>
 
 			<div className="menu-items item">
